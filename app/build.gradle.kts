d6f00eabd067cb8d6cf753e1f23eba389c0640f1
Egor <egorkrasnov123456789@gmail.com>
2025-06-09 19:31:30 +0700

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt)
+    id ("com.google.devtools.ksp") version "2.1.21-2.0.1"
+    id("com.google.dagger.hilt.android") version "2.56.2"
 }
 
 android {
@@ -47,11 +47,11 @@ android {
 dependencies {
 
     implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
 
     implementation(libs.androidx.biometric)
 
